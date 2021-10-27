e2fdf21a79e80eddaac013c3b54546db7d871d81
Jesse Rogers <jesse@jesserogers.net>
2021-10-26 19:46:34 -0700

    use XHR instead of fetch API

--- src/net/client/http/http-client.ts
+++ src/net/client/http/http-client.ts
@@ -1,65 +1,92 @@
 import { Observable, Subscriber } from "rxjs"
 import { IHttpClient } from "./http-client.interface"
 import { IHttpOptions } from "./http-options.interface"
+import { HttpRequest } from "./http-request"
 
 export class HttpClient implements IHttpClient {
 
+  public delete<T = any>(url: string, options?: IHttpOptions): Observable<T> {
+    return new Observable<T>(_observer => {
+      const _request: HttpRequest = new HttpRequest({
+        url,
+        options,
+        method: "DELETE"
+      })
+      this._request(_request, _observer)
+    })
+  }
+
   public get<T = any>(url: string, options?: IHttpOptions): Observable<T> {
     return new Observable<T>(_observer => {
-      const _request: Request = new Request(url, {
-        method: "GET",
-        credentials: options && options.credentials
+      const _request: HttpRequest = new HttpRequest({
+        url,
+        options,
+        method: "GET"
       })
-      this._processResponse(_request, _observer)
+      this._request(_request, _observer)
+    })
+  }
+
+  public patch<T = any>(url: string, body: any, options?: IHttpOptions): Observable<T> {
+    return new Observable<T>(_observer => {
+      const _request: HttpRequest = new HttpRequest({
+        url,
+        options,
+        body,
+        method: "PATCH"
+      })
+      this._request(_request, _observer)
     })
   }
 
   public post<T = any>(url: string, body: any, options?: IHttpOptions): Observable<T> {
     return new Observable<T>(_observer => {
-      const _request: Request = new Request(url, {
-        method: "POST",
-        credentials: options && options.credentials,
-        body
+      const _request: HttpRequest = new HttpRequest({
+        url,
+        options,
+        body,
+        method: "POST"
       })
-      this._processResponse(_request, _observer)
+      this._request(_request, _observer)
     })
   }
 
   public put<T = any>(url: string, body: any, options?: IHttpOptions): Observable<T> {
     return new Observable<T>(_observer => {
-      const _request: Request = new Request(url, {
-        method: "PUT",
-        credentials: options && options.credentials,
-        body
+      const _request: HttpRequest = new HttpRequest({
+        url,
+        options,
+        body,
+        method: "PUT"
       })
-      this._processResponse(_request, _observer)
+      this._request(_request, _observer)
     })
   }
 
-  public delete<T = any>(url: string, options?: IHttpOptions): Observable<T> {
-    return new Observable<T>(_observer => {
-      const _request: Request = new Request(url, {
-        method: "DELETE",
-        credentials: options && options.credentials
-      })
-      this._processResponse(_request, _observer)
-    })
-  }
-
-  private _processResponse<T = any>(_request: Request, _observer: Subscriber<T>): void {
-    fetch(_request).then(
-      _response => {
-        const _data = _response.json() as Promise<T>;
-        if (_response.status >= 200 && _response.status < 300) {
-          return _data
+  private _request(_request: HttpRequest, _observer: Subscriber<any>): void {
+    try {
+      const _xhr = new XMLHttpRequest()
+      if (_request.options && _request.options.headers)
+        this._setHeaders(_xhr, _request.options.headers)
+      _xhr.open(_request.method, _request.getFullUrl())
+      _xhr.addEventListener("load", () => {
+        switch (_xhr.responseType) {
+          case "text": _observer.next(_xhr.responseText); break
+          case "json": _observer.next(JSON.parse(_xhr.responseText)); break
         }
-        _observer.error(_data)
-      }
-    ).then(
-      _data => _observer.next(_data)
-    ).catch(
-      _err => _observer.error(_err)
-    )
+      })
+      _xhr.addEventListener("error", _error => {
+        _observer.error(_error)
+      })
+      _xhr.send()
+    } catch (_err) {
+      _observer.error(_err)
+    }
+  }
+
+  private _setHeaders(_xhr: XMLHttpRequest, _headers: { [header: string]: string }) {
+    for (const _header in _headers)
+      _xhr.setRequestHeader(_header, _headers[_header])
   }
 
 }
