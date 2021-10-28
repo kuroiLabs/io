865ce5256e901ddbfaf701e09d99e6b1127040d3
Jesse Rogers <jesse@jesserogers.net>
2021-10-27 20:33:57 -0700

    Update test-client.ts

--- tests/client/src/test-client.ts
+++ tests/client/src/test-client.ts
@@ -1,4 +1,6 @@
 import { Syringe } from "@kuroi/syringe"
+import { Observable } from "rxjs"
+import { ILobby } from "../../../src/lobby"
 import { BaseWebClient, IHttpClient } from "../../../src/net/client"
 import { HTTP } from "./http.token"
 
@@ -7,11 +9,15 @@ import { HTTP } from "./http.token"
 })
 export class TestClient extends BaseWebClient {
 
-  constructor(@Syringe.Inject(HTTP) http: IHttpClient) {
-    super(http)
+  constructor(@Syringe.Inject(HTTP) private http: IHttpClient) {
+    super()
     http.get("http://localhost:6969/api/test/leo").subscribe({
       next: res => console.log(res)
     })
+  }
+
+  public createLobby(_url: string): Observable<ILobby> {
+    return this.http.post<ILobby>(_url, null)
   }
 
 }
