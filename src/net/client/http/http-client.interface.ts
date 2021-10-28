6555da0ce8b9d9e6d0ae9158d59fd544cef92726
Jesse Rogers <jesse@jesserogers.net>
2021-10-27 19:22:56 -0700

    add patch method to interface

--- src/net/client/http/http-client.interface.ts
+++ src/net/client/http/http-client.interface.ts
@@ -3,6 +3,7 @@ import { IHttpOptions } from "./http-options.interface"
 
 export interface IHttpClient {
   get<T = any>(url: string, options?: IHttpOptions): Observable<T>
+  patch<T = any>(url: string, body: any, options?: IHttpOptions): Observable<T>
   post<T = any>(url: string, body: any, options?: IHttpOptions): Observable<T>
   put<T = any>(url: string, body: any, options?: IHttpOptions): Observable<T>
   delete<T = any>(url: string, options?: IHttpOptions): Observable<T>
