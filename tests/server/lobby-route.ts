3c5fa8c5d866c036a8bd2f6ebfe2b0ba43d009bb
Jesse Rogers <jesse@jesserogers.net>
2021-10-27 18:58:43 -0700

    inject lobby manager and pass class to super call for route config

--- tests/server/lobby-route.ts
+++ tests/server/lobby-route.ts
@@ -3,62 +3,48 @@ import { isUInt16 } from "@kuroi/numeric/typing"
 import { Syringe } from "@kuroi/syringe"
 import { Request, Response } from "express"
 import { Delete, Post } from "../../src/endpoint"
-import { ILobbyManager, Lobby } from "../../src/lobby"
+import { BaseLobbyManager } from "../../src/lobby"
 import { Route } from "../../src/route"
 import { TestLobby } from "./test-lobby"
+import { TestLobbyManager } from "./test-lobby-manager"
 
 @Syringe.Injectable()
-export class LobbyRoute extends Route implements ILobbyManager {
-
-  private lobbies = new Map<uint16, Lobby>()
+export class LobbyRoute extends Route {
 
   constructor(
-    // @todo: create an injection token for this
+    @Syringe.Inject(TestLobbyManager) private lobbyManager: BaseLobbyManager,
     private _generator = new GeneratorService()
   ) {
-    super("lobby")
-  }
-
-  public addLobby(_lobby: Lobby): void {
-    this.lobbies.set(_lobby.id, _lobby)
-  }
-
-  public removeLobby(_lobbyId: number): void {
-    this.lobbies.delete(_lobbyId)
-  }
-
-  public getLobby(_lobbyId: number): Lobby | null {
-    return this.lobbies.get(_lobbyId) || null
+    super("lobby", LobbyRoute)
   }
 
   @Post("/new")
   public newLobby(_request: Request, _response: Response) {
     try {
       const _lobby = new TestLobby({
+        name: "My Lobby",
         id: this._generator.generateNumericId(),
-        name: this._generator.randomString(),
         maxClients: 2
       })
-      this.addLobby(_lobby)
-      console.log("[LobbyRoute.newLobby] ::: Succesfully created new Lobby [" + _lobby.id + "]")
-      return _response.json(_lobby.getConfig())
+      this.lobbyManager.addLobby(_lobby)
+      _response.status(200).json(_lobby.getConfig())
     } catch (_err) {
       console.error('[LobbyRoute.newLobby] :::  uncaught exception', _err)
-      return _response.sendStatus(500).json({ error: 'Internal server error' })
+      _response.status(500).json(new Error("Internal server error"))
     }
   }
 
   @Delete("/:lobbyId")
-  public endLobby(_request: Request, _response: Response) {
+  public endLobby(_request: Request, _response: Response): void {
     const _lobbyId: uint16 = +_request.params.lobbyId
-    if (!isUInt16(_lobbyId))
-      return _response.sendStatus(400).json({
-        error: "Invalid lobby ID"
-      })
-    const _lobby = this.getLobby(_lobbyId)
+    if (!isUInt16(_lobbyId)) {
+      _response.status(400).json(new Error("Invalid lobby ID: " + _lobbyId))
+      return
+    }
+    const _lobby = this.lobbyManager.getLobby(_lobbyId)
     _lobby?.destroy()
-    this.removeLobby(_lobbyId)
-    return _response.sendStatus(200)
+    this.lobbyManager.removeLobby(_lobbyId)
+    _response.status(200).json(null)
   }
 
 }
