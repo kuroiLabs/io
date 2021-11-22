a7b0f7eaf0b94f3c3ff1649662e1241b2053d84d
Jesse Rogers <jesse@jesserogers.net>
2021-11-21 20:53:24 -0800

    Create message.service.ts

---
+++ tests/client/src/message.service.ts
@@ -0,0 +1,39 @@
+import { Syringe } from "@kuroi/syringe"
+import { TestClient } from "./test-client"
+
+@Syringe.Injectable({
+  scope: "global"
+})
+export class MessageService implements Syringe.OnInit {
+
+  private _messageDOMContainer!: HTMLElement | null
+
+  constructor(
+    @Syringe.Inject(TestClient)
+    private _client: TestClient
+  ) {
+
+  }
+
+  onInit(): void {
+    this._messageDOMContainer = document.getElementById("messages")
+  }
+
+  public addMessage(_clientId: byte, _message: string): void {
+    if (!this._messageDOMContainer) {
+      return
+    }
+    const _timestamp: Date = new Date()
+    const _messageElement: HTMLDivElement = document.createElement("div")
+    const _dateString: string = _timestamp.toLocaleDateString()
+    const _timeString: string = _timestamp.toLocaleTimeString([], {hour: 'numeric', minute:'2-digit'})
+    _messageElement.innerHTML = `<div class="message-meta"><h4>Client ${_clientId}</h4><span>${_dateString} ${_timeString}</span></div>`
+    _messageElement.innerHTML += `<div class="message-content">${_message}</div>`
+    _messageElement.classList.add("message")
+    if (_clientId !== this._client.id) {
+      _messageElement.classList.add("incoming")
+    }
+    this._messageDOMContainer.appendChild(_messageElement);
+  }
+
+}
