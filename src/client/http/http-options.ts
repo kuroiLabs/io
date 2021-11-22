6a10404adefb4bebf2d5ba02d4a404d0be9cab46
Jesse Rogers <jesse@jesserogers.net>
2021-11-21 21:50:41 -0800

    add custom property support for http options

--- src/client/http/http-options.ts
+++ src/client/http/http-options.ts
@@ -6,6 +6,8 @@ export class HttpOptions implements IHttpOptions {
 
   public credentials?: RequestCredentials
 
+  private customProperties = new Map<string, any>()
+
   public headers?: {
     [header: string]: string
   }
@@ -14,6 +16,22 @@ export class HttpOptions implements IHttpOptions {
     if (!this.headers)
       this.headers = {}
     this.headers[key] = encodeURIComponent(value)
+  }
+
+  public setValue(property: string, value: any): void {
+    this.customProperties.set(property, value)
+  }
+
+  public toJSON(): IHttpOptions {
+    const _options: IHttpOptions = {
+      contentType: this.contentType,
+      credentials: this.credentials,
+      headers: this.headers
+    }
+    this.customProperties.forEach((_value, _key) => {
+      _options[_key] = _value
+    })
+    return _options
   }
 
 }
