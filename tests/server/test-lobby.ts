17076e33a86005401c7395c2c8a5ca7a7b13dbb9
Jesse Rogers <jesse@jesserogers.net>
2021-11-21 20:56:31 -0800

    log out destroy event from test lobby

--- tests/server/test-lobby.ts
+++ tests/server/test-lobby.ts
@@ -1,13 +1,16 @@
 import WebSocket from "ws"
-import { PACKETS } from "../common/packets.enum"
-import { Lobby, ServerPacket } from "../../src/server"
 import { ILobby } from "../../src/common/lobby"
+import { Lobby, ServerPacket } from "../../src/server"
+import { PACKETS } from "../common/packets.enum"
 
 export class TestLobby extends Lobby {
 
   constructor(_lobby: ILobby) {
     super(_lobby)
     this.on(PACKETS.MESSAGE, this._onMessage.bind(this))
+    this.on("destroy", () => {
+      console.log(`Destroying lobby [${this.id}]...`)
+    })
   }
 
   public onHandshake(_client: WebSocket, _clientId: byte): void {
@@ -20,14 +23,19 @@ export class TestLobby extends Lobby {
     _client.send(_packet.data())
   }
 
-  private _onMessage(_packet: ServerPacket, _clientId: byte): void {
+  private _onMessage(_packet: ServerPacket): void {
+    const _clientId: byte = _packet.readByte()
     const _message: string = _packet.readString()
+
     console.log(`Received message from client [${_clientId}]: ${_message}`)
+
     const _bytes: Uint8Array = this.encoder.encode(_message)
     const _byteLength: int = (Uint8Array.BYTES_PER_ELEMENT * 2) + _bytes.byteLength
     const _buffer: Buffer = Buffer.alloc(_byteLength)
     const _out: ServerPacket = new ServerPacket(_buffer)
+
     _out.writeBytes([PACKETS.MESSAGE, _clientId, ..._bytes])
+
     this.clients.forEach((_client, _id) => {
       if (_id !== _clientId && _client.readyState === WebSocket.OPEN)
         _client.send(_out.data())
