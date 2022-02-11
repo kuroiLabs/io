dea886e95c8927cbeec1434c758ddfd0c1cc4437
kuro <kuro@kuroi.io>
2022-02-11 00:24:04 -0800

    register TestClient as RpcHandler

--- tests/client/src/test-client.ts
+++ tests/client/src/test-client.ts
@@ -2,23 +2,24 @@ import { Syringe } from "@kuroi/syringe"
 import { Observable } from "rxjs"
 import { take } from "rxjs/operators"
 import { IHttpClient, WebClient } from "../../../src/client"
-import { ILobby } from "../../../src/common"
+import { ILobby, RpcHandler } from "../../../src/common"
 import { HTTP } from "./http.token"
 
 @Syringe.Injectable({
-  scope: "global"
+	scope: "global"
 })
+@RpcHandler
 export class TestClient extends WebClient {
 
-  constructor(@Syringe.Inject(HTTP) private http: IHttpClient) {
-    super()
-    http.get("http://localhost:6969/api/test/leo").pipe(take(1)).subscribe({
-      next: res => console.log(res)
-    })
-  }
+	constructor(@Syringe.Inject(HTTP) private http: IHttpClient) {
+		super()
+		http.get("http://localhost:6969/api/test/leo").pipe(take(1)).subscribe({
+			next: res => console.log(res)
+		})
+	}
 
-  public createLobby(_url: string): Observable<ILobby> {
-    return this.http.post<ILobby>(_url, null)
-  }
+	public createLobby(_url: string): Observable<ILobby> {
+		return this.http.post<ILobby>(_url, null)
+	}
 
 }
