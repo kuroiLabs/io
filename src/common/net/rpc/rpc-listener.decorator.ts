e1b37d655b8a01366aa28f2c5a646ab1c8f5250d
kuro <kuro@kuroi.io>
2022-02-12 16:24:44 -0800

    add optional classname argument to wrapping RpcListener decorator

--- src/common/net/rpc/rpc-listener.decorator.ts
+++ src/common/net/rpc/rpc-listener.decorator.ts
@@ -1,36 +1,39 @@
-import { takeUntil } from "rxjs/operators";
-import { Constructor, Destroyable } from "../../utils";
-import { IPacket } from "../packet.interface";
-import { ReservedPackets } from "../reserved-packet.enum";
-import { IRpcCall } from "./rpc-call.interface";
-import { RPC_HANDLER_STORE } from "./rpc-handler-store";
-import { RpcMethods } from "./rpc-methods.symbol";
+import { takeUntil } from "rxjs/operators"
+import { Constructor, Destroyable } from "../../utils"
+import { IPacket } from "../packet.interface"
+import { ReservedPackets } from "../reserved-packet.enum"
+import { IRpcCall } from "./rpc-call.interface"
+import { RPC_HANDLER_STORE } from "./rpc-handler-store"
+import { RpcMethods } from "./rpc-methods.symbol"
 
-export function RpcListener<T extends Constructor<Destroyable>>(Target: T) {
-	class RpcListenerExtension extends Target {
-		constructor(...args: any[]) {
-			super(...args);
-			const _rpcMethods: string[] = Target.prototype[RpcMethods] || [];
-			RPC_HANDLER_STORE.get().pipe(takeUntil(this.destroyed$)).subscribe(_handler => {
-				_rpcMethods.forEach(_method => {
-					_handler.on(ReservedPackets.RPC, (_packet: IPacket<any>) => {
-						try {
-							const _rpcCallJson: string = _packet.readString();
-							const _rpcCall: IRpcCall = JSON.parse(_rpcCallJson);
-							if (_rpcCall && _rpcCall.api !== _rpcCall.api)
-								return
-	
-							(this as any)[_method](...(_rpcCall.arguments || []))
-						} catch (_err) {
-							console.error(_err)
-						}
+export function RpcListener(className?: string) {
+	return function _rpcListenerDecorator<T extends Constructor<Destroyable>>(Target: T) {
+		class RpcListenerExtension extends Target {
+			constructor(...args: any[]) {
+				super(...args)
+				const _rpcMethods: string[] = Target.prototype[RpcMethods] || [];
+				RPC_HANDLER_STORE.get().pipe(takeUntil(this.destroyed$)).subscribe(_handler => {
+					_rpcMethods.forEach(_method => {
+						_handler.on(ReservedPackets.RPC, (_packet: IPacket<any>) => {
+							try {
+								const _apiName: string = `${className || Target.name}.${_method}`;
+								const _rpcCallJson: string = _packet.readString()
+								const _rpcCall: IRpcCall = JSON.parse(_rpcCallJson)
+								if (_rpcCall && _rpcCall.api !== _apiName)
+									return
+		
+								(this as any)[_method](...(_rpcCall.arguments || []))
+							} catch (_err) {
+								console.error(_err)
+							}
+						})
 					})
 				})
-			})
+			}
 		}
+		Object.defineProperty(RpcListenerExtension, "name", {
+			value: Target.name
+		})
+		return RpcListenerExtension
 	}
-	Object.defineProperty(RpcListenerExtension, "name", {
-		value: Target.name
-	});
-	return RpcListenerExtension;
 }
