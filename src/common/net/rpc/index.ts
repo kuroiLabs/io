4376f321cbae99056928a6989edbf149324a895e
kuro <kuro@kuroi.io>
2022-02-16 21:17:29 -0800

    Update index.ts

--- src/common/net/rpc/index.ts
+++ src/common/net/rpc/index.ts
@@ -1,7 +1,9 @@
 export * from "./rpc-call.interface"
 export * from "./rpc-handler-store"
-export * from "./rpc-handler.decorator"
 export * from "./rpc-handler-store.interface"
+export * from "./rpc-handler.decorator"
+export * from "./rpc-handler.interface"
 export * from "./rpc-listener.decorator"
 export * from "./rpc-methods.symbol"
 export * from "./rpc.decorator"
+
