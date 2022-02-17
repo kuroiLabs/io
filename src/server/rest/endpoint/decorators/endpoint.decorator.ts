52adb9b644bcfaec4bbbe492718fd4f38965eb9f
kuro <kuro@kuroi.io>
2022-02-16 21:09:25 -0800

    minor semantic code change

--- src/server/rest/endpoint/decorators/endpoint.decorator.ts
+++ src/server/rest/endpoint/decorators/endpoint.decorator.ts
@@ -8,7 +8,9 @@ export function endpointDecorator(_path: string, _method: string, _guards?: Guar
 		_key: string,
 		_descriptor: PropertyDescriptor,
 	) {
-		_target[__ENDPOINTS] = _target[__ENDPOINTS] || []
+		if (!_target[__ENDPOINTS])
+			_target[__ENDPOINTS] = []
+
 		_target[__ENDPOINTS].push(new Endpoint({
 			path: _path,
 			method: _method,
