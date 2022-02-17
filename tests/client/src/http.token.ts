08b86fdca6a181b09430e2a99d1ed8e1f5aa5b50
kuro <kuro@kuroi.io>
2022-02-16 21:19:02 -0800

    Delete http.token.ts

--- tests/client/src/http.token.ts
+++
@@ -1,9 +0,0 @@
-import * as Syringe from "@kuroi/syringe"
-import { HttpClient } from "../../../src/client/http"
-
-const http = new HttpClient()
-
-export const HTTP = new Syringe.InjectionToken("HTTP", {
-  scope: "global",
-  factory: () => http
-})
