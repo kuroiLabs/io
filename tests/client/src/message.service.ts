445fac29a21e974b1fc1ede714995ab41991f322
kuro <kuro@kuroi.io>
2022-02-17 22:43:58 -0800

    Update message.service.ts

--- tests/client/src/message.service.ts
+++ tests/client/src/message.service.ts
@@ -1,4 +1,5 @@
 import * as Syringe from "@kuroi/syringe"
+import { ChatMessage } from "../../common"
 import { TestClient } from "./test-client"
 
 @Syringe.Injectable({
@@ -19,17 +20,17 @@ export class MessageService implements Syringe.OnInit {
     this._messageDOMContainer = document.getElementById("messages")
   }
 
-  public addMessage(_clientId: byte, _message: string): void {
+  public addMessage(_message: ChatMessage): void {
     if (!this._messageDOMContainer)
       return
     const _timestamp: Date = new Date()
     const _messageElement: HTMLDivElement = document.createElement("div")
     const _dateString: string = _timestamp.toLocaleDateString()
     const _timeString: string = _timestamp.toLocaleTimeString([], {hour: 'numeric', minute:'2-digit'})
-    _messageElement.innerHTML = `<div class="message-meta"><h4>Client ${_clientId}</h4><span>${_dateString} ${_timeString}</span></div>`
-    _messageElement.innerHTML += `<div class="message-content">${_message}</div>`
+    _messageElement.innerHTML = `<div class="message-meta"><h4>User ${_message.id}</h4><span>${_dateString} ${_timeString}</span></div>`
+    _messageElement.innerHTML += `<div class="message-content">${_message.message}</div>`
     _messageElement.classList.add("message")
-    if (_clientId !== this._client.id)
+    if (_message.id !== this._client.id)
       _messageElement.classList.add("incoming")
     this._messageDOMContainer.appendChild(_messageElement);
   }
