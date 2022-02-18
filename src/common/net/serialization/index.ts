89ebe6b14b5826e56213a2de8e5c24d20a5a4c71
kuro <kuro@kuroi.io>
2022-02-17 23:54:50 -0800

    rename serializable decorator file

--- src/common/net/serialization/index.ts
+++ src/common/net/serialization/index.ts
@@ -1,7 +1,6 @@
 export * from "./base-serialization-event"
 export * from "./byte-group.type"
-export * from "./deserializable.decorator"
 export * from "./deserializable.interface"
+export * from "./serializable.decorator"
 export * from "./serializable.interface"
 export * from "./serialization-event.interface"
-
