d096bd7d11db8d33eec8bc57bb763295a4783568
kuro <kuro@kuroi.io>
2022-02-17 22:41:34 -0800

    Create serialization index

---
+++ src/common/net/serialization/index.ts
@@ -0,0 +1,7 @@
+export * from "./base-serialization-event"
+export * from "./byte-group.type"
+export * from "./deserializable.decorator"
+export * from "./deserializable.interface"
+export * from "./serializable.interface"
+export * from "./serialization-event.interface"
+
